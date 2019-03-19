9589f969e7195f246598a1544f3744bf8e09f000
melbournelopes <melbournelopes@gmail.com>
2019-03-19 18:50:18 +0530

    jenkinsfile added

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,24 @@
+node {
+    stage('FETCH'){
+        git url: 'https://github.com/melbournelopes/MoneseTest.git'    
+    }
+   stage('BUILD'){
+        if (isUnix()) {
+            sh './gradlew clean build'
+        } else {
+            bat 'gradlew.bat clean build'
+        }
+   }
+   stage('TEST'){
+       echo 'testing'
+   }
+   stage('DEPLOY'){
+       //Signing apk
+       signAndroidApks (
+        keyStoreId: "imd",
+        keyAlias: "imd_keystore",
+        apksToSign: "**/*-unsigned.apk",
+        //skipZipalign: true
+        )
+   }
+}
